5dd632c2d0ad58bd77d9ed3a3998e63018144cbc
Shairyar Baig <shairyarbaig@gmail.com>
2021-04-05 18:47:46 +0500

    Create ec2 instance based on te config defined in yml file

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,10 @@
-require 'logger'
 require 'zeitwerk'
+require 'logger'
+require 'aws-sdk-ec2'
+require 'yaml'
+require 'table_print'
+require 'pry'
+
 loader = Zeitwerk::Loader.new
 loader.push_dir('./lib')
 loader.setup
--- spec/lib/spinup/aws/ec2_spec.rb
+++ spec/lib/spinup/aws/ec2_spec.rb
@@ -1,9 +1,17 @@
 describe Spinup::Aws::Ec2 do
 
-  describe "#run" do
+  describe '#run' do
     subject { described_class.new.run }
-    context "config is not available" do
-      it { is_expected.to eq(nil) }
+    xcontext 'when aws credentials are not available' do
+      before do
+        allow(ENV).to receive(:[]).with('AWS_ACCESS_KEY_ID').and_return(nil)
+        allow(ENV).to receive(:[]).with('AWS_SECRET_ACCESS_KEY').and_return(nil)
+      end
+
+      it 'raises exception' do
+        expect { subject }.to raise_exception
+      end
+
     end
   end
 end
